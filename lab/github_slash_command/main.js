13fb65d08029ec13e576a15a17cf2256cf8353c1
Eshan <eshan.trehan@rubrik.com>
2024-11-21 18:37:35 +0530

    Fix

--- lab/github_slash_command/main.js
+++ lab/github_slash_command/main.js
@@ -2,11 +2,30 @@ const { program } = require('commander');
 const { parse } = require('shell-quote');
 const { dismiss } = require('./alfred/commands');
 
+// In order to prevent treating the argument as comments we have to escape the # character
 const commandString = process.env.ISSUE_COMMENT_BODY.replaceAll("#", "\\#");
 
 const parsedCommand = parse(commandString, null, {comments: false});
-console.log("Parsed Command: ", parsedCommand);
+console.log("Parsed Slash Command: ", parsedCommand);
 
 program.addCommand(dismiss)
 
-program.parse(parsedCommand, { from: 'user' });
+program.parse(parsedCommand, { from: "user" });
+
+program.configureOutput({
+  writeOut: (str) => {
+    // Handle standard output (stdout)
+    console.log('Captured STDOUT:', str);
+    // You can also write to a file or buffer if needed
+  },
+  writeErr: (str) => {
+    // Handle standard error output (stderr)
+    console.error('Captured STDERR:', str);
+  },
+  outputError: (str, write) => {
+    // Customize error output
+    console.error('Captured ERROR:', str);
+    // Decide whether to write the error using the provided write function
+    // write(str);
+  }
+});
